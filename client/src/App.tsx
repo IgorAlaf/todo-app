390fb128b666a33e1962fbdec46c503e1a809a87
Igor <igos2006@gmail.com>
2023-07-13 22:21:03 +1000

    deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import './index.css'
-import { useAppDispatch } from './hooks/useAppDispatch'
-import { fetchCheckAuth } from './store/AsyncFunctions'
-import { useAppSelector } from './hooks/useAppSelector'
-import { IUser } from './models/IUser'
-import Todos from './components/todos/Todos'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import { useAppDispatch } from "./hooks/useAppDispatch";
+import { fetchCheckAuth } from "./store/AsyncFunctions";
+import { useAppSelector } from "./hooks/useAppSelector";
+import { IUser } from "./models/IUser";
+import Todos from "./components/todos/Todos";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const dispatch = useAppDispatch()
-  const { user, isAuth, isLoading } = useAppSelector(state => state.userReducer)
-  const [users, setUsers] = useState<IUser[]>([])
-  const [show, setShow] = useState<boolean>(true)
-  const navigate = useNavigate()
+  const dispatch = useAppDispatch();
+  const { user, isAuth, isLoading } = useAppSelector(
+    (state) => state.userReducer
+  );
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [show, setShow] = useState<boolean>(true);
+  const navigate = useNavigate();
   useEffect(() => {
     async function check() {
-      if (localStorage.getItem('token')) {
-        await dispatch(fetchCheckAuth())
+      if (!localStorage.getItem("user_id")) {
+        navigate("/auth");
+      }
+      if (localStorage.getItem("token")) {
+        await dispatch(fetchCheckAuth());
       }
     }
-    check()
-  }, [])
+    check();
+  }, []);
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
-  if (!isAuth) navigate('/auth')
+  if (!isAuth) navigate("/auth");
 
   return (
-    <div className=''>
+    <div className="">
       <Todos isAuth={isAuth} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
