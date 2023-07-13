48a0a40875a84c3aeb8cbe003df5ce0feac8fd6e
Igor <igos2006@gmail.com>
2023-07-13 17:56:50 +1000

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import './index.css'
+import { useAppDispatch } from './hooks/useAppDispatch'
+import { fetchCheckAuth } from './store/AsyncFunctions'
+import { useAppSelector } from './hooks/useAppSelector'
+import { IUser } from './models/IUser'
+import Todos from './components/todos/Todos'
+import { useNavigate } from 'react-router-dom'
+
+function App() {
+  const dispatch = useAppDispatch()
+  const { user, isAuth, isLoading } = useAppSelector(state => state.userReducer)
+  const [users, setUsers] = useState<IUser[]>([])
+  const [show, setShow] = useState<boolean>(true)
+  const navigate = useNavigate()
+  useEffect(() => {
+    async function check() {
+      if (localStorage.getItem('token')) {
+        await dispatch(fetchCheckAuth())
+      }
+    }
+    check()
+  }, [])
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (!isAuth) navigate('/auth')
+
+  return (
+    <div className=''>
+      <Todos isAuth={isAuth} />
+    </div>
+  )
+}
+
+export default App
